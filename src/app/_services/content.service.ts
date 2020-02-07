27e8fbad11966f05a940f5d52465170fb7c26b19
b_vennes <b.n.vennes@gmail.com>
2020-02-06 22:11:19 -0800

    moved api url to the environment files

--- src/app/_services/content.service.ts
+++ src/app/_services/content.service.ts
@@ -3,18 +3,17 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { ContentDataDto } from '../_dtos/content-data.dto';
 import { ContentOverviewDto } from '../_dtos/content-overview.dto';
+import { environment } from 'src/environments/environment';
 
 @Injectable()
 export class ContentService {
-    private baseurl = 'http://localhost:5000/api/content';
-
     constructor(private httpClient: HttpClient) {}
 
     public getContentData(id: string): Observable<ContentDataDto> {
-        return this.httpClient.get<ContentDataDto>(`${this.baseurl}/${id}`);
+        return this.httpClient.get<ContentDataDto>(`${environment.baseApiUrl}/${id}`);
     }
 
     public getContentOverviews(): Observable<Array<ContentOverviewDto>> {
-        return this.httpClient.get<Array<ContentOverviewDto>>(`${this.baseurl}`);
+        return this.httpClient.get<Array<ContentOverviewDto>>(`${environment.baseApiUrl}`);
     }
 }
