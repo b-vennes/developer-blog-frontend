3153a4f14dcc916aa86f9bd91c70cf4f2098914f
Branden <b.n.vennes@gmail.com>
2020-04-09 09:18:50 -0700

    Added python html post

--- src/app/_posts/posts.list.ts
+++ src/app/_posts/posts.list.ts
@@ -1,3 +1,14 @@
 import { Post } from './post.model';
 
-export const PostsList: Array<Post> = [];
+export const PostsList: Array<Post> = [
+    {
+        id: 'python-html',
+        title: 'Building an HTML Library with Python',
+        date: new Date(2020, 4, 9),
+        summary: 'Developing a Python library for building HTML templates.',
+        image: '/assets/images/divs.jpg',
+        imageAttributeUrl: 'https://unsplash.com/@flac666',
+        imageAttributeName: 'Dominik Malinowski',
+        tags: ['python', 'html', 'frontend', 'software development']
+    },
+];
