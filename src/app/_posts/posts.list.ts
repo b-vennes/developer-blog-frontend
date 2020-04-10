4b666b5ee46f88b28162ac881c8c34af2182c25f
Branden <b.n.vennes@gmail.com>
2020-04-10 16:14:44 -0700

    fix post date

--- src/app/_posts/posts.list.ts
+++ src/app/_posts/posts.list.ts
@@ -4,7 +4,7 @@ export const PostsList: Array<Post> = [
     {
         id: 'python-html',
         title: 'Building an HTML Library with Python',
-        date: new Date(2020, 4, 9),
+        date: new Date(2020, 3, 9),
         summary: 'Developing a Python library for building HTML templates.',
         image: '/assets/images/divs.jpg',
         imageAttributeUrl: 'https://unsplash.com/@flac666',
